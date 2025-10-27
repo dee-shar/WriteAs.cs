a7abcf39b00cf113464a4395ac46ab72f3184a15
huok <77536370+dee-shar@users.noreply.github.com>
2025-10-27 03:44:09 +0300

    Update WriteAs.cs

--- src/WriteAs.cs
+++ src/WriteAs.cs
@@ -8,22 +8,27 @@ namespace WriteAsApi
     public class WriteAs
     {
         private readonly HttpClient httpClient;
-        private readonly string apiUrl = "https://write.as/api";
+        private readonly string apiUrl = "https://write.as/api/";
         public WriteAs()
         {
             httpClient = new HttpClient();
-            httpClient.DefaultRequestHeaders.UserAgent.ParseAdd(
-                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36");
+            httpClient.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "Write.as v1.7.0; Android");
             httpClient.DefaultRequestHeaders.Accept.Add(
                 new MediaTypeWithQualityHeaderValue("application/x-www-form-urlencoded"));
+            httpClient.DefaultRequestHeaders.AcceptEncoding.ParseAdd("gzip");
+            httpClient.DefaultRequestHeaders.ConnectionClose = false;
         }
 
         public async Task<string> PublishText(string word)
         {
-            var content = new StringContent(
-                $"w={word}&font=norm&lang=en&rtl=false", Encoding.UTF8);
+            var content = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("w", word),
+                new KeyValuePair<string, string>("font", "norm"),
+                new KeyValuePair<string, string>("lang", "en"),
+                new KeyValuePair<string, string>("rtl", "false")
+            });
             var response = await httpClient.PostAsync(apiUrl, content);
-            response.EnsureSuccessStatusCode();
             return await response.Content.ReadAsStringAsync();
         }
     }
